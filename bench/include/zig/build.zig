c537c32f51634e1676a18b08004a9d295dff59e7
Elek <elek>
2025-01-01 19:56:02 +0100

    .

--- bench/include/zig/build.zig
+++ bench/include/zig/build.zig
@@ -5,18 +5,17 @@ pub fn build(b: *std.Build) void {
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
     // for restricting supported target set are available.
-    const target = b.host; 
-    //b.standardTargetOptions(.{});
+    const target = b.standardTargetOptions(.{});
     // Standard optimization options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
     // set a preferred release mode, allowing the user to decide how to optimize.
-    // const optimize = b.standardOptimizeOption(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
         .name = "app",
         .root_source_file = b.path("app.zig"),
         .target = target,
-        .optimize = std.builtin.Mode.ReleaseFast,
+        .optimize = optimize //std.builtin.Mode.ReleaseFast,
     });
     exe.linkLibC();
     b.installArtifact(exe);
--- bench/test_build_bench_update.sh
+++ bench/test_build_bench_update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-rm -rf build
+#rm -rf build
 
-LANGS="c zig fortran cpp "
+LANGS="python"
 PROBLEMS="mandelbrot nbody spectral-norm"
 
-dotnet run --project tool -- --task build --langs $LANGS --problems $PROBLEMS
+dotnet run --project tool -- --task build --langs $LANGS --problems $PROBLEMS --force-rebuild
 dotnet run --project tool -- --task test --langs $LANGS --problems $PROBLEMS
 dotnet run --project tool -- --task bench --langs $LANGS --problems $PROBLEMS
 
